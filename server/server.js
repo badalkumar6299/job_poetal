673b21902f5e607b31f8d659ca3179f7ab82317d
Badal Kumar <badalkumar55950@gmail.com>
2025-03-19 15:01:03 +0530

    Updated User model and server configurations

--- server/server.js
+++ server/server.js
@@ -32,7 +32,7 @@ app.use((err, req, res, next) => {
 });
 
 // Server Setup
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 
 Sentry.setupExpressErrorHandler(app);
 app.listen(PORT, () => {
